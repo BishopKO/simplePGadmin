b0e1df1b8f30b504d7443c10bac09aaf7100fcb2
Adam Jaskowiak <aj.jaskowiak@gmail.com>
2020-05-17 19:23:32 +0100

    TEST

--- src/components/organisms/DatabasesList/DatabasesList.stories.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import DatabasesList from './DatabasesList';
-
-storiesOf('Organisms/DatabasesList', module).add('Normal', () => <DatabasesList />);
